618524221b8fc462760c314e6f4586b5158b3320
p2pcmlp <nnaing@stuy.edu>
2017-04-20 10:56:29 -0400

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,11 @@
+public class Ticket {
+
+    private int vipLevel;
+    private String description;
+    private String name;
+    private boolean resolved;
+    private String solnDescription;
+    private int ID;
+
+
+}
