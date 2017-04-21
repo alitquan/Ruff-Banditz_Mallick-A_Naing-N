4096aad6549c952048fdc88241084ef4ddcd7200
Alitquan <amallick@stuy.edu>
2017-04-21 01:25:31 -0400

    Added methods to Ticket for Comparable

--- Ticket.java
+++ Ticket.java
@@ -7,5 +7,31 @@ public class Ticket {
     private String solnDescription;
     private int ID;
 
+    public Ticket (int vip, String desc, String nombre,String soln, int iden) {
+	vipLevel = vip;
+	description = desc;
+	name = nombre;
+	resolved = false;
+	solnDescription = soln;
+	ID = iden;
+    }
 
+    public int getVip() {
+	return vipLevel;
+    }
+    
+    public int compareTo(Ticket other) {
+	if (this.getVip() > other.getVip()) 
+	    return 1;
+	else if (this.getVip() == other.getVip() )
+	    return 0;
+	else
+	    return -1;
+    }
+
+    public static void main (String [] args) {
+	Ticket a = new Ticket (23,"computer won't turn on","a","replace battery",1234);
+	Ticket b = new Ticket (12,"computer screen is broken","b","get a new one",5678);
+	System.out.println (a.compareTo(b));
+    }
 }
