cdd55e8b3bf25e41e8d15871690386e84580a6be
p2pcmlp <nnaing@stuy.edu>
2017-04-23 23:26:36 -0400

    Update HelpDesk.java

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,4 +1,11 @@
 public class HelpDesk {
 	private ArrayPriorityQueue<Ticket> listofTickets;
-
+	public void addTicket(Ticket t) {
+		listofTickets.add(t);
+	}
+	public Ticket removeMinTicket() {
+		Ticket result = listofTickets.peekMin();
+		listofTcikets.removeMin();
+		return result;
+	}
 }
